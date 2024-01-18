23185560e7e61cf544a36206e35d3c1234591375
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2024-01-18 20:47:47 +0000

    feat: add footer content

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,18 @@
+import { CiLinkedin } from 'react-icons/ci';
+import { AiFillGithub } from 'react-icons/ai';
+
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
-	return <footer className={styles.footerContainer}>© Lee Charlton-Hassall</footer>;
+	return <footer className={styles.footerContainer}>
+		<div className={styles.socialLinks}>
+			<a href='https://www.linkedin.com/in/leehassall/' target='_blank' rel="noreferrer">
+				<CiLinkedin color='#ffece5' size='25px' />
+			</a>
+			<a href='https://github.com/LeeCharltonH' target='_blank' rel="noreferrer">
+				<AiFillGithub color='#ffece5' size='25px' />
+			</a>
+		</div>
+		<span className={styles.copyright}>© Lee Charlton-Hassall </span>
+	</footer>;
 };
