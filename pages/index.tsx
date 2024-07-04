128dca5ec271a60c1b2db676a316dfcf9abf36c8
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2024-07-04 20:35:57 +0100

    refactor: remove skills endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
 import { MetaTags } from 'Components/Layout/MetaTags/MetaTags';
-import { getKeySkills } from 'Services/api/keySkills';
-import { GetStaticProps } from 'next';
 import { Homepage } from 'Components/Pages/Homepage/Homepage';
 
-export const getStaticProps = (async () => {
-	let { data } = await getKeySkills();
-
-	data = JSON.parse(JSON.stringify(data));
-	
-	return { props: { keySkills: data } };
-}) satisfies GetStaticProps<{
-	keySkills: string[];
-}>;
 
 export default function Styles() {
 	return (
