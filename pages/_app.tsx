17b9429c7da8f3ed41b60b0ac537d579bb8a2873
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2024-07-12 17:03:40 +0100

    fix font layout shift

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import styles from '../styles/App.module.scss';
 import 'Styles/globals.scss';
 import { PageComposer } from 'Components/Layout/PageComposer/PageComposer';
 import { useIsDeviceSize } from 'Hooks/useIsDeviceSize';
+import { Proza_Libre } from '@next/font/google';
+import classNames from 'classnames';
+
+const prozaLibre = Proza_Libre({
+	subsets: ['latin', 'latin-ext'],
+	weight: ['400', '500', '700'],
+});
 
 export const App = ({ Component, pageProps }: AppProps) => {
 	const { isMobile } = useIsDeviceSize();
 
-	return <div className={styles.container}>
+	return <div className={classNames(styles.container, prozaLibre.className)}>
 		<PageComposer sidebar={!isMobile}>
 			<Component {...pageProps} />
 		</PageComposer>
